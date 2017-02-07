35871d0bb2f7fcefcf74466bd8e03124c9d4b0a1
Phong Phan <fongfan999@gmail.com>
2017-02-07 07:31:51 +0700

    Fix background-attachment on mobile brownser

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+  $(".button-collapse").sideNav({ edge: 'right' });
+
+  // Smooth scroll anchor link
+  $(document).on('click', 'a', function(event){
+    if ($.attr(this, 'href').charAt(0) == '#') {
+     event.preventDefault();
+
+     $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+   }
+ });
+});
+
+$('.persistent').click(function(e) {
+  e.preventDefault();
+});
+
+$('.close-side-nav').click(function(e) {
+  e.preventDefault();
+  $('.button-collapse').sideNav('hide');
+});
